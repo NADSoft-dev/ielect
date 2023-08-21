aff3937300e847a982caee73c7931845c504be88
hadeel banimatar <96240714+hadeelBanimattar@users.noreply.github.com>
2023-08-21 13:25:17 +0300

    update style

--- app/Http/Controllers/ElectorsController.php
+++ app/Http/Controllers/ElectorsController.php
@@ -384,10 +384,15 @@ class ElectorsController extends Controller
     }
       // print_r ($listFields);
       $all_sub=DB::table('groups')->where('category_id',$idfilter)->get();
+     
       $all_array_sub=[];
       
       foreach($all_sub as $sub){
         array_push($all_array_sub,$sub->id);
+      }
+      $sub_sub=DB::table('groups')->whereIn('category_id',$all_array_sub)->get();
+      foreach($sub_sub as $sub2){
+        array_push($all_array_sub,$sub2->id);
       }
       // print_r($all_array_sub);
       $electors=SELF::buildQuery($filter)->orWhereIn('group',$all_array_sub);
