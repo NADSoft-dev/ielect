5e29d19eed476fc0842335ba1b61277af1a43a27
ibtihalsh <ibtihal@nadsoft.net>
2023-08-24 21:45:02 +0300

    update2

--- app/Http/Controllers/ElectorsController.php
+++ app/Http/Controllers/ElectorsController.php
@@ -163,6 +163,24 @@ class ElectorsController extends Controller
           unset($filter[$key]);
           break;
         }
+
+
+        if($f['name']=="group"){
+          $all_sub=DB::table('groups')->where('category_id',$f['value'])->get();
+        
+          $all_array_sub=[];
+          
+          foreach($all_sub as $sub){
+            array_push($all_array_sub,$sub->id);
+          }
+          $sub_sub=DB::table('groups')->whereIn('category_id',$all_array_sub)->get();
+          foreach($sub_sub as $sub2){
+            array_push($all_array_sub,$sub2->id);
+          }
+          // print_r($all_array_sub);
+          $electors=$electors->orWhereIn('group',$all_array_sub);
+        }
+
         
       }
     
@@ -375,12 +393,12 @@ class ElectorsController extends Controller
         // }
       $filter=json_decode(Request::get('filter'),true);
       
-      if(count($filter)>1){
-        // print_r($filter[1]['name']);
-        $idfilter=$filter[1]['value'];
-        $namefilter=$filter[1]['name'];
-      // print_r(count($filter));
-      }
+      // if(count($filter)>1){
+      //   // print_r($filter[1]['name']);
+      //   $idfilter=$filter[1]['value'];
+      //   $namefilter=$filter[1]['name'];
+      // // print_r(count($filter));
+      // }
       
     }else{
       $filter=Request::cookie('electorsFilter');
@@ -396,26 +414,26 @@ class ElectorsController extends Controller
       $listFields=json_decode($listFields,true);
     }
       // print_r ($listFields);
-      if(isset($namefilter) && $namefilter =='group'){
-        // print_r($filter[1]['name']);
-           $all_sub=DB::table('groups')->where('category_id',$idfilter)->get();
+      // if(isset($namefilter) && $namefilter =='group'){
+      //   // print_r($filter[1]['name']);
+      //      $all_sub=DB::table('groups')->where('category_id',$idfilter)->get();
         
-          $all_array_sub=[];
+      //     $all_array_sub=[];
           
-          foreach($all_sub as $sub){
-            array_push($all_array_sub,$sub->id);
-          }
-          $sub_sub=DB::table('groups')->whereIn('category_id',$all_array_sub)->get();
-          foreach($sub_sub as $sub2){
-            array_push($all_array_sub,$sub2->id);
-          }
-          // print_r($all_array_sub);
-          $electors=SELF::buildQuery($filter)->orWhereIn('group',$all_array_sub);
-          // print_r($electors);
-      }
-      else{
+      //     foreach($all_sub as $sub){
+      //       array_push($all_array_sub,$sub->id);
+      //     }
+      //     $sub_sub=DB::table('groups')->whereIn('category_id',$all_array_sub)->get();
+      //     foreach($sub_sub as $sub2){
+      //       array_push($all_array_sub,$sub2->id);
+      //     }
+      //     // print_r($all_array_sub);
+      //     $electors=SELF::buildQuery($filter)->orWhereIn('group',$all_array_sub);
+      //     // print_r($electors);
+      // }
+      // else{
         $electors=SELF::buildQuery($filter);
-      }
+      // }
       
       // $electors=SELF::buildQuery($filter);
       $electors=$electors->paginate($pageCount);
