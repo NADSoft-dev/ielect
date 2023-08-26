bed712e77da084176f5f8a79af197455ac9e103b
ibtihalsh <ibtihal@nadsoft.net>
2023-08-26 20:14:06 +0300

    update3

--- app/Http/Controllers/ElectorsController.php
+++ app/Http/Controllers/ElectorsController.php
@@ -255,13 +255,13 @@ class ElectorsController extends Controller
             $sub_sub=DB::table('groups')->whereIn('category_id',$all_array_sub)->get();
             foreach($sub_sub as $sub2){
               array_push($all_array_sub,$sub2->id);
-              array_push($sub,$sub2->id);
+              $sub_sub_sub=DB::table('groups')->where('category_id',$sub2->id)->get();
+              foreach($sub_sub_sub as $sub3){
+                array_push($all_array_sub,$sub3->id);
+              }
             }
 
-            $sub_sub_sub=DB::table('groups')->whereIn('category_id',$sub)->get();
-            foreach($sub_sub_sub as $sub3){
-              array_push($all_array_sub,$sub3->id);
-            }
+            
 
             array_push($all_array_sub,$f['value']);
             // print_r($all_array_sub);
